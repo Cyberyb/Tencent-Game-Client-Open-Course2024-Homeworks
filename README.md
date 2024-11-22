d4c0fb5edabb623881c019cb8662205b1a97598e
Cybery <cyberybhshu@163.com>
2024-11-22 19:32:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tencent-Game-Client-Open-Course2024-Homeworks
+腾讯2024游戏客户端公开课作业
---
+++ Source/AndroidProject/AndroidProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AndroidProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAndroidProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAndroidProjectGameMode();
+};
+
+
+
